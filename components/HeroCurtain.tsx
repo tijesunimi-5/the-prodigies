26b67af7e6c5dbd58b2d835acc27df7fa0c75b7f
tijesunimi-5 <tijesunimiidowu16@gmail.com>
2026-03-08 20:03:28 +0100

    fixed the hero curtain

--- components/HeroCurtain.tsx
+++ components/HeroCurtain.tsx
@@ -46,7 +46,7 @@ export default function HeroCurtain() {
           </p>
         </motion.div>
 
-        <ScrollIndicator progress={scrollYProgress} />
+        <ScrollIndicator  />
       </div>
     </section>
   );
