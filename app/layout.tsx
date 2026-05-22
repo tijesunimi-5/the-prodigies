22ec415c6d42a3aa2d850841c18113759a647ced
tijesunimi-5 <tijesunimiidowu16@gmail.com>
2026-05-22 12:21:09 +0100

    added the css back

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
+import "./globals.css";
 import FloatingActions from "@/components/FloatAction";
 import AuthOverlay from "@/components/auth/AuthOverlay";
 
